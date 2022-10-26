f6e12b302362aeaf6e8be0b48e3e629d62d054d8
Juan181803 <jframirez.valencia@gmail.com>
2022-10-26 13:15:22 -0500

    FileChooser

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 import com.google.gson.Gson;
 import model.Controller;
 import model.Reader;
-
+import javax.swing.JFileChooser;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -218,31 +218,106 @@ public class Main {
         }
 
     }
+    public static void readerCommand(String[] splitedComand) {
+
+        for (String mostrar : splitedComand) {
+
+            System.out.println(mostrar);
+
+        }
+
+        if (splitedComand.length < 4) {
+            throw new RuntimeException("Command wrong written");
+        }
+
+        try {
+
+            if (splitedComand[0].equals("INSERT") && splitedComand[1].equals("INTO")) {
+
+                if (splitedComand[2].equalsIgnoreCase("countries(id,") && splitedComand[6].equals("VALUES")) {
+
+                    String countryid = splitedComand[7].substring(2, 38);
+                    System.out.println("Contry id: " + countryid);
+                    String countryname = splitedComand[8].substring(1, (splitedComand[8].length() - 2));
+                    System.out.println("Contry name: " + countryname);
+                    String countrycode = splitedComand[10].substring(1, (splitedComand[10].length() - 2));
+                    System.out.println("Contry code: " + countrycode);
+                    try {
+                        double countrypop = Double.parseDouble(splitedComand[9].substring(0, (splitedComand[9].length() - 1)));
+                        System.out.println("Contry population: " + countrypop);
+                        if (cn.addCountry(countryid, countryname, countrypop, countrycode)) {
+                            System.out.println("The country was added");
+                        } else {
+                            System.out.println("The country wans't added");
+                        }
+
+                    } catch (NumberFormatException ex) {
+                        ex.printStackTrace();
+                    }
+
+
+                } else if (splitedComand[2].equalsIgnoreCase("cities(id,") && splitedComand[6].equals("VALUES")) {
+
+                    String cityid = splitedComand[7].substring(2, 38);
+                    System.out.println("City id: " + cityid);
+                    String cityname = splitedComand[8].substring(1, (splitedComand[8].length() - 2));
+                    System.out.println("city name: " + cityname);
+                    String countrycode = splitedComand[9].substring(1, 37);
+                    System.out.println("Contry code: " + countrycode);
+                    if (!cn.searchCountryCode(countrycode)) {
+                        throw new RuntimeException("That country dont exist");
+                    }
+                    try {
+                        double citypop = Double.parseDouble(splitedComand[10].substring(0, (splitedComand[10].length() - 1)));
+                        System.out.println("City population: " + citypop);
+                        if (cn.addCity(cityid, cityname, citypop, countrycode)) {
+                            System.out.println("the city was added");
+                        } else {
+                            System.out.println("the city wasn't added");
+                        }
+
+                    } catch (NumberFormatException ex) {
+                        ex.printStackTrace();
+                    }
+
+                }
+
+
+            }
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
 
     public static void reader() {
+        JFileChooser buscador = new JFileChooser();
+        buscador.showOpenDialog(buscador);
         File archivo = null;
         FileReader fr = null;
         BufferedReader br = null;
         String file = "";
 
         try {
-
-            archivo = new File("DataBase.sql");
+            String patch = buscador.getSelectedFile().getAbsolutePath();
+            archivo = new File(patch);
             fr = new FileReader(archivo);
             br = new BufferedReader(fr);
 
-            String com = "";
             String linea;
             String[] splitedComand = new String[0];
             while ((linea = br.readLine()) != null) {
 
-                com = linea;
+                String com = linea;
                 splitedComand = com.split(" ");
                 System.out.println(linea);
-
-
+                readerCommand(splitedComand);
 
             }
+            /*
 
 
             for (String mostrar : splitedComand) {
@@ -378,7 +453,7 @@ public class Main {
                 ex.printStackTrace();
                 System.out.println("Command wrong written");
 
-            }
+            }*/
 
 
         } catch (Exception e) {
